f04e258414c0d124fec106e176118073a8bc0304
SamuelPalenik111128 <xpaleniks@stuba.sk>
2022-02-18 11:28:51 +0100

    lists

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,10 +2,23 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("5 + 7 = " + App.scitaj(5, 7));
-    }
+        System.out.println("Dlzka: " + args.length);
 
-    static int scitaj(int a, int b){
-        return a + b;
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+
+        int[] pole = new int[10];
+        for(int i = 0; i < pole.length; i++){
+            if(i == 0){
+                pole[i] = 1;
+                continue;
+            }
+            pole[i] = pole[i - 1] * 2;
+        }
+
+        for(int i = 0; i < pole.length; i++){
+            System.out.println(i + " : " + pole[i]);
+        }
     }
 }
