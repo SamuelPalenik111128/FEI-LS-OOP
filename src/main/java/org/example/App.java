2838ea5e3f2b81a4f9b6f09b6a5aeaaab2ee7550
SamuelPalenik111128 <xpaleniks@stuba.sk>
2022-02-18 10:22:22 +0100

    Hello World

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,5 +4,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("Welcome!\n");
+
+        int celeCislo = 5;                      // 5 -> 32 bit
+        long celeCisloLong = 5L;                // 5 -> 64 bit
+        float singlePrecisionDesatinne = 5f;
+        double doublePrecisionDesatinne = 5.5;
+        char znak = 'H';
+        String retazec = "Hello World";
+        boolean logical = true;
+
+        int podiel = 2/3;   // =0
+        int modulo = 2%3;   // =2
     }
 }
