9b91be9be7964fd748e739077099ded92ad061f3
SamuelPalenik111128 <xpaleniks@stuba.sk>
2022-02-18 11:13:26 +0100

    while and do-while cycle

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,35 +2,18 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        int num1 = 15;
-        float num2 = 3.5f;
-        float result;
 
-        if ((result = (num1 / num2))> 2.0f) {
-            System.out.println("Statement is true, result = " + result);
-        }
-        else {
-            System.out.println("Statement is false, result =  " + result);
+        int a = 0;
+        int b = 0;
+
+        while (a < 5) {
+            System.out.println("a = " + a);
+            a++;
         }
 
-        int a = 5;
-
-        for(int i = 0; i <= a; i++) {
-            switch (i) {
-                case 1:
-                    System.out.println("a je jedna");
-                    break;
-                case 2:
-                    System.out.println("a je dva");
-                    break;
-                case 3:
-                case 4:
-                    System.out.println("a je tri ale bo styri");
-                    break;
-                default:
-                    System.out.println(String.format("a je %d", i));
-                    break;
-            }
-        }
+        do {
+            System.out.println("b = " + b);
+            b++;
+        } while (b < 2);
     }
 }
