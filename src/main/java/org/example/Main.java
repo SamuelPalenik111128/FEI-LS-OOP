6c9cf5c4a3cec688a163180ca1536e1f5aeec263
SamuelPalenik111128 <xpaleniks@stuba.sk>
2022-02-23 16:17:50 +0100

    methods

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int[] y = new int[]{1, 2, 3};
+
+        System.out.println(x);
+        testPrimitive(x);
+        System.out.println(Arrays.toString(y));
+        testArray(y);
+        System.out.println(Arrays.toString(y));
+    }
+
+    public static void testPrimitive(int cislo){
+        cislo = 10;
+        System.out.println(cislo);
+    }
+
+    public static void testArray(int[] array){
+        array[0] = 10;
+        System.out.println(Arrays.toString(array));
+    }
+}
